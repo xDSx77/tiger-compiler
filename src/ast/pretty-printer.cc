8f85b125ccb7a6368a16eecc5dbdaab96e191dff
Lucas Brun <lucas.brun@epita.fr>
2019-02-15 22:23:32 +0100

    [AST] pretty-printer.cc for dementor

--- src/ast/pretty-printer.cc
+++ src/ast/pretty-printer.cc
@@ -48,7 +48,7 @@ namespace ast
   void
   PrettyPrinter::operator()(const FieldVar& e)
   {
-  // FIXME: Some code was deleted here.
+    ostr_ << e.var_get() << e.name_get();
   }
 
   /* Foo[10]. */
@@ -71,6 +71,119 @@ namespace ast
     ostr_ << "_cast(" << e.exp_get() << ", " << e.ty_get() << ')';
   }
 
-  // FIXME: Some code was deleted here.
+  // faire ca pour toute les fonctions
+  void
+  PrettyPrinter::operator()(const CallExp e)
+  {
+      ostr_ << e.body_get() << e.dec_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const 0pExp e)
+  {
+      ostr_ << e.left_get() << e.right_get();
+  }
+
+
+  void
+  PrettyPrinter::operator()(const RecordExp e)
+  {
+      ostr_ << e.field_get() << e.right_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const SeqExp e)
+  {
+      ostr_ << e.exp_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const IfExp e)
+  {
+      ostr_ << e.test_get() << e.body_get() << e.body_2_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const WhileExp e)
+  {
+    ostr_ << e.test_get() << e.body_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const ForExp e)
+  {
+      ostr_ << e.vardec_get() << e.hi_get() << e.body_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const LetExp e)
+  {
+      ostr_ << e.decs_get() << e.body_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const ArrayExp e)
+  {
+    ostr_ << e.exp_get() << e.type_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const CastExp e)
+  {
+    ostr_ <<  e.exp_get() << e.ty_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const FieldInit e)
+  {
+      ostr_ << e.init_get();
+  }
+
+  void
+  PrettyPrinter::operator()(const DecsList e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const Decs e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const VarDecs e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const FunctionDecs e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const TypeDec e)
+
+  void
+  PrettyPrinter::operator()(const TypeDecs e)
+
+  void
+  PrettyPrinter::operator()(const NameTy e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const RecordTy e)
+  {
+  }
+
+  void
+  PrettyPrinter::operator()(const ArrayTy e)
+  {
+      ostr_ << e.base_type_get();
+  }
+  void
+  PrettyPrinter::operator()(const Field e)
+  {
+      ostr_ << e.type_name_get();
+  }
 
 } // namespace ast
