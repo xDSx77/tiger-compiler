c57ce6c73f8d6d475d2669a19b4542bcc5991284
Allan Cantin <allan.cantin@epita.fr>
2019-02-23 13:53:17 +0100

    [TEST-SUITE] added a test-suite with basic tests

---
+++ test-suite.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+function test_good()
+{
+  ./src/tc $file
+  if [ $(echo $?) = 0 ]; then
+    echo -e "\033[32;1m[OK] $file \033[0m"
+  else
+    echo -e "\033[31;1m[KO] $file \033[0m"
+  fi
+}
+
+function test_error()
+{
+  ./src/tc $file
+  i=$(echo $?)
+  if [ $i = 3 ] || [ $i = 2 ]; then
+    echo -e "\033[32;1m[OK] error detected in $file \033[0m"
+  else
+    echo -e "\033[31;1m[KO] error not detected in $file \033[0m"
+  fi
+}
+
+echo "===================================================="
+echo "================TESTING GOOD FILES=================="
+echo "===================================================="
+for file in ./tests/good/*.tig ; do
+  test_good
+done
+
+echo "===================================================="
+echo "===========TESTING SYNTAX ERROR IN FILES============"
+echo "===================================================="
+for file in ./tests/syntax/*.tig ; do
+  test_error
+done
+
+
