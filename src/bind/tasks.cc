2fd5e965e36a86a8213a9fb0d12cc03b3f3df1f0
Frederic Boucart <frederic.boucart@epita.fr>
2019-03-02 17:10:27 +0100

    [BIND] make tasks but comment them to compile

--- src/bind/tasks.cc
+++ src/bind/tasks.cc
@@ -2,5 +2,29 @@
  ** \file bind/tasks.cc
  ** \brief Bind module tasks implementation.
  */
+/*
+#include <misc/contract.hh>
+#include <bind/libbind.hh>
+#define DEFINE_TASK 3
+#include <bind/tasks.hh>
+#undef DEFINE_TASK
+#include <common.hh>
+#include <ast/libast.hh>
+#include <ast/tasks.hh>
 
-  // FIXME: Some code was deleted here.
+namespace bind::tasks
+{
+    void
+    bindings_display()
+    {
+        ast::bindings_display(std::cout) = true;
+    }
+
+    void
+    bindings_compute()
+    {
+        task_error() << ::bind::bind(*ast::tasks::the_program);
+        task_error().exit_on_error();
+    }
+}
+*/
--- src/bind/tasks.hh
+++ src/bind/tasks.hh
@@ -3,4 +3,20 @@
  ** \brief Bind module related tasks.
  */
 
-  // FIXME: Some code was deleted here.
+/*
+#pragma once
+
+#include <task/libtask.hh>
+
+namespace bind::tasks
+{
+    TASK_GROUP("3. Binding");
+
+    ///Dispaly the  bindings
+    TASK_DECLARE("b|bind-display","display the binding",
+        bindings_compute,"parse");
+
+    ///Dispaly the  bindings
+    TASK_DECLARE("B|bind-display","display the binding",
+            bindings_display,"parse");
+}//namespace bind::tasks*/
